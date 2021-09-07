5025d2490d92bca09fabe50043a5824bd8423626
Kishore Kumar Panem <kishore4171@gmail.com>
2021-09-07 10:30:07 +0530

    server info

--- cookbooks/mtfirstcookbook/recipes/server.rb
+++ cookbooks/mtfirstcookbook/recipes/server.rb
@@ -1,9 +1,10 @@
 file '/etc/motd' do
-    content 'System Information
+    content "System Information
         IP APPDRESS : #{node['ipaddress']}
         hostname: #{node['ipaddress']}
-        kernel release: #{node['kernel']}
+        kernel release: #{node['kernel']['release']}
         fqdn: #{node['fqdn']}
-        operating system: #{node['operating_system']}
-        sda1 disk platform: #{node['/dev/sda1']} '
+        operating system: #{node['hostnamectl']['operating_system']}
+        sda1 disk platform: #{node['filesystem']['by_device']['/dev/sda1']}
+        "
 end
