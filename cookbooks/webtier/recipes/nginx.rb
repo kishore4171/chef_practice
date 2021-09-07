94fb6bb6d9c7350bc3d8e49a52d5743094692c0b
kishore <kishore4171@gmail.com>
2021-09-07 22:36:05 +0530

    first try

---
+++ cookbooks/webtier/recipes/nginx.rb
@@ -0,0 +1,25 @@
+package node.default['package_name']['webserver'] do
+    action :install
+end
+
+service 'nginx' do
+    action [ :start, :enable ]
+end
+
+execute 'backup_index_html' do
+    command 'cp /usr/share/doc/HTML/index.html /usr/share/doc/HTML/index.html.orig'
+    action :run
+end
+
+template '/usr/share/doc/HTML/index.html' do
+    source 'index.html.erb'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+    variables(webserver_name: node.default['package_name']['webserver'][1])
+end
+
+service 'nginx' do
+    action [ :start, :enable ]
+end
