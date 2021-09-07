f5a4bb318ba67759f13cbda86c6f49288e6e46f1
kishore <kishore4171@gmail.com>
2021-09-07 11:47:42 +0530

    added service

---
+++ cookbooks/mtfirstcookbook/attributes/default.rb
@@ -0,0 +1,5 @@
+if node['platform'] == "centos"
+    node.default[package_name] = 'httpd'
+else
+    node.default[package_name] = 'apache2'
+end
--- cookbooks/mtfirstcookbook/recipes/default.rb
+++ cookbooks/mtfirstcookbook/recipes/default.rb
@@ -5,3 +5,14 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 
 include_recipe "mtfirstcookbook::server"
+pkg = node.default['package_name']
+
+package pkg do
+    action :install
+end
+
+service 'service_name' do
+    action [:enable, :start]
+end
+
+
