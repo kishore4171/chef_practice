d7851ece39bf60875c0fbb8c6ad5e85bda9014ae
kishore <kishore4171@gmail.com>
2021-09-08 07:38:32 +0530

    server cookbook

--- cookbooks/server/recipes/default.rb
+++ cookbooks/server/recipes/default.rb
@@ -3,3 +3,53 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+
+cookbook_file '/etc/yum.repos.d/nginx.repo' do
+    source 'nginx.repo'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
+
+package 'httpd' do
+    action :remove
+end
+
+package 'nginx' do
+    action :install
+end
+
+directory '/testweb' do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
+
+template '/var/www/html/hello.html' do
+    source 'hello.html.erb'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
+
+template '/testweb/practice.conf' do
+    source 'practice.erb'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
+
+execute 'setenforce' do
+    command 'setenforce 0'
+    action :run
+end
+
+service 'nginx' do
+    action [ :start, :enable ]
+end
+
+
